53d40d8cdf5bc7b2766874332f08b9181b569745
prothegee <prothegee@proton.me>
2025-12-06 15:15:30 +0700

    seperate

--- backend_cc_drogon/main.cc
+++ backend_cc_drogon/main.cc
@@ -3,11 +3,16 @@
 #define ADDRESS_IP "0.0.0.0"
 #define ADDRESS_PORT 9003
 
+// --------------------------------------------------------- //
+
+// --------------------------------------------------------- //
+
 int main() {
 	printf("backend_cc_drogon: run on %s:%d\n", ADDRESS_IP, ADDRESS_PORT);
 
 	drogon::app().addListener(ADDRESS_IP, ADDRESS_PORT);
 
+	//
 
 	drogon::app().run();
 
