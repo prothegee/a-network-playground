d52c781bef1d2619d71480bf8d86e40ba1e42dc4
prothegee <prothegee@proton.me>
2025-12-30 15:59:11 +0700

    add parse path

--- backend_go/main.go
+++ backend_go/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 const ADDRESS = "0.0.0.0:9004"
@@ -14,11 +16,18 @@ const ADDRESS = "0.0.0.0:9004"
 
 const HTTP_CONTENT_TYPE_HINT = "Content-Type"
 
-const HTTP_CONTENT_TYPE_HINT_APP_TEXT = "text/plain"
-const HTTP_CONTENT_TYPE_HINT_APP_JSON = "application/json"
+const HTTP_CONTENT_TYPE_HINT_TEXT_PLAIN = "text/plain"
+const HTTP_CONTENT_TYPE_HINT_APPLICATION_JSON = "application/json"
 
+const HTTP_RESP_MESSAGE_NOT_FOUND = "Method Not Allowed"
 const HTTP_RESP_MESSAGE_METHOD_NOT_ALLOWED = "Method Not Allowed"
 const HTTP_RESP_MESSAGE_INTERNAL_SERVER_ERROR = "Internal Server Error"
+
+// --------------------------------------------------------- //
+
+var (
+	validValuePattern = regexp.MustCompile("^[a-zA-Z0-9_-]+$")
+)
 
 // --------------------------------------------------------- //
 
@@ -39,7 +48,7 @@ func JsonHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set(HTTP_CONTENT_TYPE_HINT, HTTP_CONTENT_TYPE_HINT_APP_JSON)
+	w.Header().Set(HTTP_CONTENT_TYPE_HINT, HTTP_CONTENT_TYPE_HINT_APPLICATION_JSON)
 
 	resp := TypeRespJson {
 		Decimal: 3.14,
@@ -62,7 +71,38 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	resp := "home"
 
-	w.Header().Set(HTTP_CONTENT_TYPE_HINT, HTTP_CONTENT_TYPE_HINT_APP_TEXT)
+	w.Header().Set(HTTP_CONTENT_TYPE_HINT, HTTP_CONTENT_TYPE_HINT_TEXT_PLAIN)
+
+	io.WriteString(w, resp)
+}
+
+// n/a
+func DynamicHandler1(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, HTTP_RESP_MESSAGE_METHOD_NOT_ALLOWED, http.StatusMethodNotAllowed)
+		return
+	}
+
+	pathParts := strings.Split(r.URL.Path, "/")
+
+	// maybe check len of pathParts and check each pathParts index then do something, i.e.:
+	/*
+	if len(pathParts) != 4 || pathParts[0] != "" || pathParts[1] != "go" ... and so on ... {
+		http.Error(w, "Woa, Not Found", http.StatusNotFound)
+		return
+	}
+	*/
+	// or check depth for each pathParts as in the len
+
+	epn2 := pathParts[2]
+	if epn2 == "" || !validValuePattern.MatchString(epn2) {
+		http.Error(w, HTTP_RESP_MESSAGE_NOT_FOUND, http.StatusNotFound)
+		return
+	}
+
+	resp := fmt.Sprintf("this is /%s/%s\n", pathParts[1], epn2)
+
+	w.Header().Set(HTTP_CONTENT_TYPE_HINT, HTTP_CONTENT_TYPE_HINT_TEXT_PLAIN)
 
 	io.WriteString(w, resp)
 }
@@ -72,6 +112,20 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 func registerHandler() {
 	http.HandleFunc(HomeHandlerPath, HomeHandler)
 	http.HandleFunc(JsonHandlerPath, JsonHandler)
+	http.HandleFunc("/go/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/go" {
+			HomeHandler(w, r)
+			return
+		}
+		if r.URL.Path == "/go/json" {
+			JsonHandler(w, r)
+			return
+		}
+		if strings.HasSuffix(r.URL.Path, "") {
+			DynamicHandler1(w, r)
+			return
+		}
+	})
 }
 
 // --------------------------------------------------------- //
