720ceddcef0abb627ac00ff67d9df3ce93e4cb84
prothegee <prothegee@proton.me>
2026-02-17 22:02:39 +0700

    modified build function script

--- env.sh
+++ env.sh
@@ -21,6 +21,13 @@ function _build_backend_cc_drogon() {
 # --------------------------------------------------------- #
 # --------------------------------------------------------- #
 
+function _build_backend_rust() {
+    rustc ./backend_rust/src/main.rs -o ./backend_rust.o -C opt-level=3;
+}
+
+# --------------------------------------------------------- #
+# --------------------------------------------------------- #
+
 function _build_backend_go() {
 	go build -o ./backend_go.o ./backend_go;
 }
@@ -29,4 +36,13 @@ function _build_backend_go_fiber() {
 	go build -o ./backend_go_fiber.o ./backend_go_fiber;
 }
 
+# --------------------------------------------------------- #
+# --------------------------------------------------------- #
 
+function _build_backend_all() {
+    _build_backend_cc;
+    _build_backend_cc_drogon;
+    _build_backend_go;
+    _build_backend_go_fiber;
+    _build_backend_rust;
+}
